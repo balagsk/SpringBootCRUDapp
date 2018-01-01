1e2227641aff62d5d0d79ae08080b0472cd7ad42
Balakrishana <balaskn@gmail.com>
2018-01-02 01:30:17 +0800

    Author name updated

--- src/main/java/com/developer/SpringMySQL/controllers/MainController.java
+++ src/main/java/com/developer/SpringMySQL/controllers/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Created by Pukar on 6/25/2017.
+ * Created by Balakrishan on 6/25/2017.
  */
 @Controller
 public class MainController {
--- src/main/java/com/developer/SpringMySQL/models/AppUsersRepo.java
+++ src/main/java/com/developer/SpringMySQL/models/AppUsersRepo.java
@@ -3,7 +3,7 @@ package com.developer.SpringMySQL.models;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- * Created by Pukar on 6/25/2017.
+ * Created by Balakrishan on 6/25/2017.
  */
 public interface AppUsersRepo extends CrudRepository<AppUsers,Integer>{
 }
